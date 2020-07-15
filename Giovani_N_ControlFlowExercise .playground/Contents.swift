e4f32459adf54bffa75fb8ed0d7b408c271ed9a8
GiovaniNunez <67969074+GiovaniNunez@users.noreply.github.com>
2020-07-15 11:32:56 -0500

    Add files via upload

---
+++ Giovani_N_ControlFlowExercise .playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var someInts = [Int] ()
+
+someInts.append (10)
+
+print("My Collection List Has \(someInts.count) items")
+
+var Collectionlist: [String]  = ["Funko Pop Figures", "Figures", "Video Games", "Plushies", "Posters", "Starbucks  Cups", "I used to collect Legos", "Books", ]
+
+for item in Collectionlist {
+    print(item)
+    
+    
+}
+
